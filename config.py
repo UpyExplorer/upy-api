2eccbb9c952dfce08a8f59d0886e14dafd260f1b
Fernando Celmer <fernando.celmer@cakeerp.com>
2021-12-12 04:46:21 -0300

    UPY-#7 - Update ​​development and production environment variables

--- config.py
+++ config.py
@@ -1,17 +1,24 @@
 import os
+import environ
 from flask_swagger_ui import get_swaggerui_blueprint
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))  
-DEBUG = os.environ.get("DEBUG")
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+env = environ.Env()
+environ.Env.read_env(BASE_DIR+"/.env")
+
+DEBUG = env("DEBUG")
+PORT = env("PORT")
+HOST = env("HOST")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+SQLALCHEMY_DATABASE_URI = env("SQLALCHEMY_DATABASE_URI")
 DATABASE_CONNECT_OPTIONS = {}
 THREADS_PER_PAGE = 2
 
 CSRF_ENABLED = True
-CSRF_SESSION_KEY = os.environ.get("CSRF_SESSION_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+CSRF_SESSION_KEY = env("CSRF_SESSION_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 SWAGGER_URL = '/docs'
 API_URL = BASE_DIR + "/staticfiles/swagger.json"
--- start.py
+++ start.py
@@ -2,8 +2,8 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    debug = os.environ.get("DEBUG", False)
-    host = os.environ.get("HOST", "localhost")
-    
-    app.run(host=host, port=port, debug=debug)
+    app.run(
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=app.config['DEBUG']
+    )
