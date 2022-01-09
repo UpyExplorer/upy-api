9f286c59ee25146222e9e74b609ed2866bc2ff92
FernandoCelmer <email@fernanocelmer.com>
2022-01-09 06:04:40 -0300

    No Task - Structure update

--- app/conf/development.py
+++ app/conf/development.py
@@ -2,7 +2,7 @@ import os
 import environ
 from os.path import dirname
 
-BASE_DIR = dirname(dirname(dirname(dirname(os.path.abspath(__file__)))))
+BASE_DIR = dirname(dirname(dirname(os.path.abspath(__file__))))
 
 env = environ.Env()
 environ.Env.read_env(BASE_DIR+"/.env")
@@ -13,9 +13,6 @@ HOST = env("HOST")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 SQLALCHEMY_DATABASE_URI = env("SQLALCHEMY_DATABASE_URI")
-
-DATABASE_CONNECT_OPTIONS = {}
-THREADS_PER_PAGE = 2
 
 CSRF_ENABLED = False
 CSRF_SESSION_KEY = env("CSRF_SESSION_KEY")
--- start.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import app
-
-if __name__ == "__main__":
-    app.run(
-        host=app.config['HOST'],
-        port=app.config['PORT'],
-        debug=app.config['DEBUG']
-    )
