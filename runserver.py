3748f961d2d29a851a8f83bb19aa9177e0a2d30c
FernandoCelmer <email@fernanocelmer.com>
2022-01-15 22:43:45 -0300

    No Task - Update runserver

--- runserver.py
+++ runserver.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     host = app.config['HOST']
     port = app.config['PORT']
 
-    if bool(app.config['DEBUG']) == True:
+    if app.config['DEBUG'] == True:
         serve(app, host=host, port=port)
     else:
         app.run(host=host, port=port)
